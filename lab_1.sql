353a9d51535a6b92e4c1e054b509d2ab4ba7bf27
Arafat Hossen Patwary <arafathn1299@gmail.com>
2025-01-27 03:40:31 +0600

    Add files via upload

---
+++ lab_1.sql
@@ -0,0 +1,13 @@
+CREATE TABLE student_info(
+ roll INT,
+ name VARCHAR(50),
+ dept VARCHAR(20)
+ 
+);
+
+INSERT INTO student_info(roll,name,dept) VALUES (1,"ARAFAT","CSE"),
+(2,"ABUSAYED","CSE"),
+(3,"JEKEY","CSE"),
+(4,"MEHEDI","CSE");
+SELECT * FROM student_info;
+
